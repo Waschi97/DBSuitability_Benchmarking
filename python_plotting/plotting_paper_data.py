e7f893b1b7dfd5af34134f162f11661042895c1a
Tom Waschischeck <tom.waschischeck@web.de>
2020-07-24 11:24:06 +0200

    plotting paper vs openMS data

--- python_plotting/plotting_paper_data.py
+++ python_plotting/plotting_paper_data.py
@@ -2,30 +2,25 @@ import os
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
 from pathlib import Path
 
 def readCSVdata(File):
     InFile = open(File, 'r')
     data_table = csv.reader(InFile, delimiter="\t")
 
-    db_names = []
-    db_suit = []
-    db_hits = []
-
+    suit_map = {}
+    
     header = True
     for row in data_table:
         if header:
             header = False
             continue
-        db_names.append(Path(row[1]).stem)
-        db_suit.append(float(row[2]))
-        db_hits.append(int(row[3]))
+        suit_map[Path(row[1]).stem] = (float(row[2]), int(row[3]))
 
     InFile.close()
     
-    db_suit, db_hits, db_names = (list(t) for t in zip(*sorted(zip(db_suit, db_hits, db_names), reverse=True)))
-    
-    return db_names, db_suit, db_hits
+    return suit_map
 
 #-------------------------------------------------------------------------------------
 # input
@@ -33,26 +28,57 @@ def readCSVdata(File):
 paper_data = f"..{os.path.sep}python_calculating{os.path.sep}paper_workflow_out.txt"
 openMS_data = f"..{os.path.sep}python_calculating{os.path.sep}openMS_workflow_out.txt"
 
-paper_names, paper_suit, paper_hits = readCSVdata(paper_data)
-openMS_names, openMS_suit, openMS_hits = readCSVdata(openMS_data)
+paper_map = readCSVdata(paper_data)
+openMS_map = readCSVdata(openMS_data)
 
-pos = list(range(len(paper_suit)))
+suit_paper = []
+suit_openMS = []
+hits_paper = []
+hits_openMS = []
+names = []
+for key in paper_map:
+    if key in openMS_map:
+        names.append(key[8:])
+        suit_paper.append(paper_map[key][0])
+        suit_openMS.append(openMS_map[key][0])
+        hits_paper.append(paper_map[key][1])
+        hits_openMS.append(openMS_map[key][1])
+    else:
+        print(f"Skipped {key} from paper output because it wasn't found in the OpenMS output.")
+
+
+suit_paper, suit_openMS, hits_paper, hits_openMS, names = (list(t) for t in zip(*sorted(zip(suit_paper, suit_openMS, hits_paper, hits_openMS, names), reverse=True)))
+
+pos = list(range(len(suit_paper)))
 width = 0.25
+
+params = {'legend.fontsize': 'medium',
+          'figure.figsize': (10, 6),
+         'axes.labelsize': 'medium',
+         'axes.titlesize':'large',
+         'xtick.labelsize':'small',
+         'ytick.labelsize':'medium'}
+pylab.rcParams.update(params)
 
 fig, ax = plt.subplots()
 
-plt.bar(pos, paper_suit, width, alpha=0.5, color='b')
-plt.bar([p + width for p in pos] , openMS_suit, width, alpha=0.5, color='r')
+plt.bar(pos, suit_paper, width, alpha=0.5, color='b')
+plt.bar([p + width for p in pos] , suit_openMS, width, alpha=0.5, color='r')
 
+ax.set_yticks(list(np.arange(0,1.05,0.05)))
 ax.set_ylabel('Suitability')
+ax.yaxis.grid(linestyle='dotted')
 
 ax.set_xticks([p + 0.5 * width for p in pos])
-ax.set_xticklabels([name for name in paper_names], rotation='vertical')
+ax.set_xticklabels([name for name in names], rotation=35)
 
 plt.ylim([0, 1])
 
 plt.legend(['Paper', 'OpenMS'], loc='upper right')
 
-plt.show()
+plt.title("Suitability of Various FASTA Files for Analysis\nof LC-MS/MS Data from Human Tryptic Peptides")
 
-InFile.close()
+plt.tight_layout()
+
+plt.savefig("paper_vs_openms.png")
+
