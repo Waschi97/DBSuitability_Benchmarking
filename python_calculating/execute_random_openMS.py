2031f4c0483a725a0ed584ca201c6d31032ca545
Tom Waschischeck <tom.waschischeck@web.de>
2020-12-18 17:00:21 +0100

    dbsuit now uses the correct database - no spikes around 0.5

--- python_calculating/execute_random_openMS.py
+++ python_calculating/execute_random_openMS.py
@@ -28,14 +28,14 @@ db = r"C:\Development\data\db\for_human_analysis\uniprot_human.FASTA"
 crap_db = r"C:\Development\data\db\crap.FASTA"
 
 # output file
-out = "openMS_random_portion_out.txt"
+out = "corrected_suitability_human.txt"
 
 # wanted FDR(s)
 FDRs = [0.01]
 
 # set the number of samples you wish to be done
 # percentages are calculated to be equidistant
-num_test = 1
+num_test = 10
 
 # Set to 'True' if you want to keep the temporary files produced by this script
 keep_files = False
@@ -50,8 +50,10 @@ ratios = ratios[::-1] # reverse the list
 start = time.perf_counter()
 ratio_to_suit = {}
 for ratio in ratios:
-    num_iterations = round(1/ratio)
+    num_iterations = round(1/ratio) * 2
+
     print(f"Starting with ratio: {ratio}\n")
+
     i = 0
     while i < num_iterations:
         i += 1
@@ -66,26 +68,24 @@ for ratio in ratios:
         print("Done!\n")
           
         # combine db with novo protein and build decoy database
-    
-        decoy_db = f"{Path.cwd()}{os.path.sep}{Path(database).stem}_decoy.FASTA"
-        novo_db = f"{Path.cwd()}{os.path.sep}novo_decoy.FASTA"
-        print("Calculating decoy database(s) ...")
-        os.system(f"DecoyDatabase -in {database} {crap_db} -out {decoy_db}")
-        os.system(f"DecoyDatabase -in {novo_fasta} -out {novo_db}")
+        
+        combined_db = f"{Path.cwd()}{os.path.sep}{Path(database).stem}_novo_combined.FASTA"
+        print("Calculating decoy database ...")
+        os.system(f"DecoyDatabase -in {database} {crap_db} {novo_fasta} -out {combined_db}")
         print("Done!\n")
   
         # run CometAdapter
 
         idXML = f"{Path.cwd()}{os.path.sep}{Path(mzML).stem}.idXML"
         print("Running CometAdapter ...")
-        os.system(f"CometAdapter -in {mzML} -out {idXML} -database {decoy_db} -comet_executable {comet_exe} -precursor_mass_tolerance 20.0 -isotope_error 0/1/2/3 -missed_cleavages 2 -num_hits 10 -max_precursor_charge 6 -spectrum_batch_size 15000 -threads {comet_threads}")
+        os.system(f"CometAdapter -in {mzML} -out {idXML} -database {combined_db} -comet_executable {comet_exe} -precursor_mass_tolerance 20.0 -isotope_error 0/1/2/3 -missed_cleavages 2 -num_hits 10 -max_precursor_charge 6 -spectrum_batch_size 15000 -threads {comet_threads}")
         print("Done!\n")
 
         # run PeptideIndexer
 
         indexed_idXML = f"{Path(idXML).stem}_indexed.idXML"
         print("Running PeptideIndexer ...")
-        os.system(f"PeptideIndexer -in {idXML} -out {indexed_idXML} -fasta {decoy_db}")
+        os.system(f"PeptideIndexer -in {idXML} -out {indexed_idXML} -fasta {combined_db}")
         print("Done!\n")
 
         # run DatabaseSuitability
@@ -93,7 +93,7 @@ for ratio in ratios:
         for FDR in FDRs:
             DBSuit_out = f"db_suit_out_{FDR}.tsv"
             print(f"Running DatabaseSuitability with FDR {FDR}...")
-            os.system(f"DatabaseSuitability -in_id {indexed_idXML} -in_novo {novo_seqs} -in_spec {mzML} -database {decoy_db} -novo_database {novo_db} -out {DBSuit_out} -cut_off_fract 0.99 -FDR {FDR} -debug 1")
+            os.system(f"DatabaseSuitability -in_id {indexed_idXML} -in_novo {novo_seqs} -in_spec {mzML} -database {database} -novo_database {novo_fasta} -out {DBSuit_out} -algorithm:reranking_cutoff_percentile 0.01 -algorithm:FDR {FDR}")
             print("Done!\n")
 
             # extract wanted data from output
@@ -107,11 +107,11 @@ for ratio in ratios:
             
             data_table = csv.reader(data_file, delimiter="\t")
 
-            db_hits = -1
-            novo_hits = -1
-            corr_novo_hits = -1
-            suitability = -1
-            corr_suitability = -1
+            db_hits = " "
+            novo_hits = " "
+            corr_novo_hits = " "
+            suitability = " "
+            corr_suitability = " "
             for row in data_table:
                 if row[0] == "#top_db_hits":
                     db_hits = int(row[1])
@@ -124,7 +124,7 @@ for ratio in ratios:
                 if row[0] == "corrected_suitability":
                     corr_suitability = float(row[1])
 
-            if db_hits < 0 or novo_hits < 0 or corr_novo_hits < 0 or suitability < 0 or corr_suitability < 0:
+            if db_hits == " " or novo_hits == " " or corr_novo_hits == " " or suitability == " " or corr_suitability == " ":
                 print("Couldn't find data in DatabaseSuitability output .. Aborting!")
                 exit()
 
@@ -144,8 +144,7 @@ for ratio in ratios:
         if not keep_files:
             print("Removing files ...")
             os.remove(database)
-            os.remove(decoy_db)
-            os.remove(novo_db)
+            os.remove(combined_db)
             os.remove(idXML)
             os.remove(indexed_idXML)
             print("Done!\n")
@@ -154,7 +153,7 @@ output = open(out, 'w')
 output.write("ratio,FDR\t[suitability, corrected_suitability, #db_hits, #novo_hits, #corrected_novo_hits]*N\n")
 for key in ratio_to_suit:
     result_list = ratio_to_suit[key]
-    line = f"{key}\t"
+    line = f"{key}"
     avg_suit = 0
     avg_db_hits = 0
     avg_novo_hits = 0
